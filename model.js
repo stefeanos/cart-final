157387f4c58aa2d92e1f072b3c671691b23702c3
stefeanos <84009599+stefeanos@users.noreply.github.com>
2021-10-11 09:36:30 +0200

    Initial commit

---
+++ model.js
@@ -0,0 +1,57 @@
+const model = {
+
+    cart: [],
+
+
+
+
+    product: [
+        {
+
+            name: "Antikk Sofa",
+            img: "dj steve 1 (19).jpeg",
+            tag: "antikksofa",
+            price: 15000,
+            desc: `Komfortabel sofa`,
+            quantity: 0,
+
+        },
+        {
+            name: "Stol I Stoff",
+            img: "dj steve 1 (5).jpg",
+            tag: "stolistoff",
+            price: 20000,
+            desc: `Fin og komfortabel sofa`,
+            quantity: 0,
+
+        },
+        {
+            name: "Sofa Antikk",
+            img: "dj steve 1 (20).png",
+            tag: `sofaantikk`,
+            price: 4000,
+            desc: `Komfortabel lenestol`,
+            quantity: 0,
+
+
+        },
+        {
+            name: "Stol I Sofa",
+            img: "dj steve 1 (9).jpg",
+            tag: "stolisofa",
+            price: 7000,
+            desc: `Fin og komfortabel sofa`,
+            quantity: 0,
+
+        },
+    ],
+};
+
+
+console.log(model);
+
+
+
+
+
+
